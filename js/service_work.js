80d5f9807cd2513822804ad80a862cec66bcf3d3
Michel Felix <36669696+michelfelix@users.noreply.github.com>
2023-08-24 22:53:53 -0300

    Adicionado os ícones de favicon para os dispositivos e código teste para pwa

---
+++ js/service_work.js
@@ -0,0 +1,9 @@
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+    .then(function() {
+        console.log('Service worker registered');
+    })
+    .catch(function() {
+        console.warn('Service worker failed');
+    });
+}
