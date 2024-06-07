eb5f20a42d64f12909cff95a215ff9996162a0a7
Michel <michel.dias@envisiontecnologia.com.br>
2024-06-07 12:01:42 -0300

    Adicionado novo projeto, userslikeme é um projeto já montado em angular anteriormente mas passei aqui com js vanilla

---
+++ projects/userslikeme/js/script.js
@@ -0,0 +1,59 @@
+var currentUser
+
+function getInfoUser() {
+  fetch("https://randomuser.me/api/")
+    .then((resp) => resp.json())
+    .then(function (data) {
+      let user = data.results;
+      currentUser = user;
+      var backgroundImage = document.getElementById("photoBackground");
+      return user.map((user) => {
+        var userFirstName = user.name.first;
+        var userLastName = user.name.last;
+        var userLocationCity = user.location.city;
+        var userLocationCountry = user.location.country;
+        var userPhotoPicture = user.picture.large;
+        var userBornAt = user.location.country;
+        var userAge = user.dob.age;
+        var userEmail = user.email;
+        var userCellPhone = user.cell;
+
+        document.getElementById("userName").innerHTML = [
+          userFirstName,
+          userLastName,
+        ].join(" ");
+
+        document.getElementById("country").innerHTML = [
+          userLocationCity,
+          userLocationCountry,
+        ].join(", ");
+
+        document.getElementById(
+          "photoPicture"
+        ).innerHTML = `<img class="user-photo" src="${userPhotoPicture}">`;
+
+        backgroundImage.style.backgroundImage = `URL(${userPhotoPicture})`;
+
+        document.getElementById("bornAt").innerHTML = `born at: ${userBornAt}`;
+
+        document.getElementById("age").innerHTML = `Age: ${userAge} years old`;
+
+        document.getElementById("email").innerHTML = `email: ${userEmail}`;
+
+        document.getElementById("phone").innerHTML = `Phone1: ${userCellPhone}`;
+      });
+    });
+}
+
+function store(key, data) {
+  if (localStorage.getItem(key) === null) {
+    localStorage.setItem(key, JSON.stringify([data]));
+  }
+  else {
+    localStorage.setItem(key, JSON.stringify([ ...JSON.parse(localStorage.getItem(key)), data ]))
+  }
+}
+
+function followUser() {
+  store("users", currentUser)
+}
